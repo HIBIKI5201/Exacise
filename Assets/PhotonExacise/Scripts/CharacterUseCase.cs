588ce51ff85c83bd205a0928f8361e42fd92d853
HIBIKI_5201 <takuto5201@gmail.com>
2025-10-21 19:01:15 +0900

    [fix]エンティティを保存し、idから出力するように

--- Assets/PhotonExacise/Scripts/CharacterUseCase.cs
+++ Assets/PhotonExacise/Scripts/CharacterUseCase.cs
@@ -1,9 +1,9 @@
 using Fusion;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace PhotonExacise.Scripts
 {
-    /*
     public class CharacterUseCase : NetworkBehaviour
     {
         private static CharacterUseCase _instance;
@@ -11,33 +11,37 @@ namespace PhotonExacise.Scripts
         [RuntimeInitializeOnLoadMethod]
         private static void Init()
         {
-            
+            _instance = null;
+        }
+
+        public static void Register(CharacterEntity entity)
+        {
+            _instance._entities.TryAdd(entity.Id, entity);
         }
 
         // ---- 攻撃要求を送る（クライアントから呼ばれる）----
         public static void Attack(CharacterEntity entity, int baseDamage)
         {
             // クライアントがホストにRPCで攻撃を要求
-            _instance.RPC_RequestAttack(entity, baseDamage);
+            _instance.RPC_RequestAttack(entity.Id, baseDamage);
         }
 
         // ---- クライアント→ホスト ----
         [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
-        private void RPC_RequestAttack(CharacterEntity entity, int baseDamage)
+        private void RPC_RequestAttack(string id, int baseDamage)
         {
             int finalDamage = CalcDamage(baseDamage);
 
-            // ダメージを適用（ホストで計算・反映）
-            entity.TakeDamage(finalDamage);
-
             // 全員に「攻撃結果」を通知
-            RPC_NotifyAttackResult(entity, finalDamage);
+            RPC_NotifyAttackResult(id, finalDamage);
         }
 
         // ---- ホスト→全クライアント ----
         [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
-        private void RPC_NotifyAttackResult(CharacterEntity entity, int finalDamage)
+        private void RPC_NotifyAttackResult(string id, int finalDamage)
         {
+            if (!_instance._entities.TryGetValue(id, out CharacterEntity entity)) return;
+
             Debug.Log($"[Notify] {entity} に {finalDamage} のダメージが適用されました");
 
             // クライアント側も表示・エフェクトなどを再生可能
@@ -47,10 +51,24 @@ namespace PhotonExacise.Scripts
         // ---- ダメージ計算 ----
         private int CalcDamage(int baseDamage)
         {
-            int result = baseDamage + Random.Range(-3, 3);
+            int result = baseDamage + 3;
             Debug.Log($"[ServerCalc] 結果ダメージ: {result}");
             return result;
         }
+
+        private Dictionary<string, CharacterEntity> _entities = new();
+
+        public override void Spawned()
+        {
+            base.Spawned();
+
+            if (_instance == null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            _instance = this;
+        }
     }
-    */
 }
