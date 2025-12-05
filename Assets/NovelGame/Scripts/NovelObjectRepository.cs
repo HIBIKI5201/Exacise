618f9b735f0a527c9d94702112459d9aa8fd99fc
HIBIKI_5201 <takuto5201@gmail.com>
2025-12-05 17:08:49 +0900

    [update]リポジトリにノベルUI系を追加

--- Assets/NovelGame/Scripts/NovelObjectRepository.cs
+++ Assets/NovelGame/Scripts/NovelObjectRepository.cs
@@ -7,17 +7,21 @@ namespace NovelGame.Scripts
     {
         public NovelObjectRepository(
             BackGroundUIManager backGroundUIManager,
-            CharacterAnimator[] characterAnimators
+            CharacterAnimator[] characterAnimators,
+            MassageWindowPresenter massageWindowPresenter
             )
         {
             _backGroundUIManager = backGroundUIManager;
             _characterAnimators = characterAnimators;
+            _massageWindowPresenter = massageWindowPresenter;
         }
 
         public BackGroundUIManager BackGroundUIManager => _backGroundUIManager;
         public CharacterAnimator[] CharacterAnimators => _characterAnimators;
+        public MassageWindowPresenter MassageWindowPresenter => _massageWindowPresenter;
 
         private readonly BackGroundUIManager _backGroundUIManager;
         private readonly CharacterAnimator[] _characterAnimators;
+        private MassageWindowPresenter _massageWindowPresenter;
     }
 }
