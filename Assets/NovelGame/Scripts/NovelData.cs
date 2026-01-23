c81a65fe047679c26e15aeb15ecd57bcb39b45d1
HIBIKI_5201 <takuto5201@gmail.com>
2026-01-23 15:54:54 +0900

    [add]NovelDataにコンストラクタを追加

--- Assets/NovelGame/Scripts/NovelData.cs
+++ Assets/NovelGame/Scripts/NovelData.cs
@@ -9,6 +9,11 @@ namespace NovelGame.Scripts
     [CreateAssetMenu(fileName = nameof(NovelData), menuName = nameof(NovelData), order = 0)]
     public class NovelData : ScriptableObject
     {
+        public NovelData(TextData[] textDatas)
+        {
+            _textDatas = textDatas;
+        }
+
         public TextData this[int index] => _textDatas[index];
         public int Length => _textDatas.Length;
         public TextData[] TextDatas => _textDatas;
@@ -29,6 +34,14 @@ namespace NovelGame.Scripts
                 _text = text;
             }
 
+            public TextData(string text, string name, bool isWaitForInput, IAction[] actionObjects)
+            {
+                _text = text;
+                _name = name;
+                _isWaitForInput = isWaitForInput;
+                _actionObject = actionObjects;
+            }
+
             public string Name => _name;
             public string Text => _text;
             public bool IsWaitForInput => _isWaitForInput;
@@ -38,10 +51,10 @@ namespace NovelGame.Scripts
             private string _name = string.Empty;
             [SerializeField, TextArea(3, 10)]
             private string _text = string.Empty;
-            [SerializeReference, SubclassSelector]
-            private IAction[] _actionObject = null;
             [SerializeField]
             private bool _isWaitForInput = true;
+            [SerializeReference, SubclassSelector]
+            private IAction[] _actionObject = null;
         }
     }
 }
