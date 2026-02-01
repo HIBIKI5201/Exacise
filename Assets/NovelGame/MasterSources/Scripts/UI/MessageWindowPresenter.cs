8e443fadfefed84ba765714334a93b1f1a6f9a04
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-01 19:50:15 +0900

    [update]ボタンの判定を追加

--- Assets/NovelGame/MasterSources/Scripts/UI/MessageWindowPresenter.cs
+++ Assets/NovelGame/MasterSources/Scripts/UI/MessageWindowPresenter.cs
@@ -1,17 +1,23 @@
 using System;
 using UnityEngine.UIElements;
 using UnityEngine;
+using NovelGame.Master.Scripts.Utility;
 
 namespace NovelGame.Master.Scripts.UI
 {
     [Serializable]
     public class MessageWindowPresenter
     {
+        public event Action OnButtonClicked;
+
         public VisualElement CreateView(MessageWindowViewModel vm)
         {
             _root = _visualTreeAsset.Instantiate();
             RootStyleInit(_root.style);
             _root.dataSource = vm;
+
+            _clickButton = _root.Q<Button>(ClickButtonName);
+            ClickButtonInit(_clickButton);
 
             return _root;
         }
@@ -19,7 +25,11 @@ namespace NovelGame.Master.Scripts.UI
         [SerializeField]
         private VisualTreeAsset _visualTreeAsset;
 
+        [SerializeField]
+        private string ClickButtonName = "click-button";
+
         private VisualElement _root;
+        private Button _clickButton;
 
         private void RootStyleInit(IStyle style)
         {
@@ -27,6 +37,16 @@ namespace NovelGame.Master.Scripts.UI
             style.flexGrow = 1;
             style.width = Length.Percent(100);
             style.height = Length.Percent(100);
+        }
+
+        private void ClickButtonInit(Button button)
+        {
+            button.clicked += OnClickButton;
+        }
+
+        private void OnClickButton()
+        {
+            OnButtonClicked?.Invoke();
         }
     }
 }
