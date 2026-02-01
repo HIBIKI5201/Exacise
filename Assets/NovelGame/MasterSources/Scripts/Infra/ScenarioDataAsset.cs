52ec4151efb5b5874600ab1faf196a122e1a93b1
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-01 17:32:00 +0900

    [update]シナリオデータにノードデータを追加

--- Assets/NovelGame/MasterSources/Scripts/Infra/ScenarioDataAsset.cs
+++ Assets/NovelGame/MasterSources/Scripts/Infra/ScenarioDataAsset.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 namespace NovelGame.Master.Scripts.Infra
 {
     /// <summary>
-    /// ノベルゲームのデータアセットを表します。
+    ///     ノベルゲームのデータアセットを表します。
     /// </summary>
     [CreateAssetMenu(fileName = nameof(ScenarioDataAsset), 
         menuName = InfraContraint.ASSET_PATH + nameof(ScenarioDataAsset))]
     public class ScenarioDataAsset : ScriptableObject
     {
+        public ScenarioNode this[int index] => _scenarioNodes[index];
 
+        public void SetScenarioNodes(ScenarioNode[] scenarioNodes)
+        {
+            _scenarioNodes = scenarioNodes;
+        }
+
+        [SerializeField]
+        private ScenarioNode[] _scenarioNodes;
     }
 }
