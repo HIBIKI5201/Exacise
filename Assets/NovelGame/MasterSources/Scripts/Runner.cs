6532e2682b460b71be70b756385bc5f884151649
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-01 18:48:10 +0900

    [add]ランナーにデータアセットを追加

--- Assets/NovelGame/MasterSources/Scripts/Runner.cs
+++ Assets/NovelGame/MasterSources/Scripts/Runner.cs
@@ -1,3 +1,5 @@
+using NovelGame.Master.Scripts.Infra;
+using NovelGame.Master.Scripts.Presenter;
 using NovelGame.Master.Scripts.UI;
 using UnityEngine;
 
@@ -6,12 +8,28 @@ namespace NovelGame.Master.Scripts.Runner
     public class Runner : MonoBehaviour
     {
         [SerializeField]
+        private ScenarioDataAsset _scenarioAsset;
+        [SerializeField]
+        private ActorAssetDataBase _actorDataBase;
+
+        [SerializeField]
         private NovelUIPresenter _novelUIPresenter;
         [SerializeField]
         private BackGroundPresenter _bgPresenter;
 
         private void Start()
         {
+            if (_scenarioAsset == null) 
+            {
+                Debug.LogError("ScenarioDataAssetが設定されていません。");
+                return; 
+            }
+            if (_actorDataBase == null)
+            {
+                Debug.LogError("ActorAssetDataBaseが設定されていません。");
+                return;
+            }
+
             if (_novelUIPresenter == null)
             { _novelUIPresenter = FindAnyObjectByType<NovelUIPresenter>(); }
             if (_novelUIPresenter == null)
@@ -19,6 +37,11 @@ namespace NovelGame.Master.Scripts.Runner
 
             MessageWindowViewModel messageWindowVM = ScriptableObject.CreateInstance<MessageWindowViewModel>();
             _novelUIPresenter.BindMessageWindowViewModel(messageWindowVM);
+
+            ScenarioPlayer scenarioPlayer = new ScenarioPlayer(
+                asset: _scenarioAsset,
+                repo: new ActionRepository(_novelUIPresenter, _bgPresenter, _actorDataBase),
+                pause: null);
         }
     }
 }
