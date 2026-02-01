274428f3dcacfaeb79cfa505bbddd56ebfdbd114
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-01 18:47:32 +0900

    [fix]旧名前空間を削除

--- Assets/NovelGame/MasterSources/Scripts/UI/BackGroundPresenter.cs
+++ Assets/NovelGame/MasterSources/Scripts/UI/BackGroundPresenter.cs
@@ -1,9 +1,9 @@
+using NovelGame.Master.Scripts.Utility;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
-using NovelGame.Scripts;
 
 namespace NovelGame.Master.Scripts.UI
 {
