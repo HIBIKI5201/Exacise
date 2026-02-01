4e9bce34e3aa8d50aeeeee91d011aba98774952a
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-01 17:38:42 +0900

    [update]名前空間を更新

--- Assets/NovelGame/Editor/NovelGameSettingsProvider.cs
+++ Assets/NovelGame/Editor/NovelGameSettingsProvider.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-namespace NovelGame.Scripts.Editor
+namespace NovelGame.Master.Scripts.Editor
 {
     public class NovelGameSettingsProvider : SettingsProvider
     {
