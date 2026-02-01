147bb0a06adc9b9f3072bd84e501594ee5110d6b
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-02 00:51:27 +0900

    [add]シナリオログ用のVMを追加

---
+++ Assets/NovelGame/MasterSources/Scripts/UI/ScenarioLogWindowViewModel.cs
@@ -0,0 +1,44 @@
+using NovelGame.Master.Scripts.Infra;
+using NovelGame.Master.Scripts.Presenter;
+using UnityEditor.MemoryProfiler;
+using UnityEngine;
+
+namespace NovelGame.Master.Scripts.UI
+{
+    public class ScenarioLogWindowViewModel : ScriptableObject
+    {
+        public void Bind(ScenarioDataAsset scenario, 
+            ScenarioPlayer player)
+        {
+            _scenario = scenario;
+            _player = player;
+
+            _scenarioNodeViewModels = new ScenarioNodeViewModel[_scenario.Length];
+            for (int i = 0; i < _scenario.Length; i++)
+            {
+                ScenarioNode node = _scenario[i];
+                _scenarioNodeViewModels[i] = new(node.Name, node.Text);
+            }
+        }
+
+        public ScenarioNodeViewModel this[int index] => _scenarioNodeViewModels[index];
+        public ScenarioNodeViewModel[] DisplayedNodes => _scenarioNodeViewModels[.._player.CurrentIndex];
+
+        public class ScenarioNodeViewModel
+        {
+            public ScenarioNodeViewModel(string name, string message)
+            {
+                Name = name;
+                Message = message;
+            }
+
+            public readonly string Name;
+            public readonly string Message;
+        }
+
+        private ScenarioNodeViewModel[] _scenarioNodeViewModels;
+
+        private ScenarioDataAsset _scenario;
+        private ScenarioPlayer _player;
+    }
+}
