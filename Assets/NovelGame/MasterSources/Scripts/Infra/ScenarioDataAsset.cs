c6bd863b5afa46312702b1ac71cfccf5569d0a11
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-01 17:05:38 +0900

    [add]シナリオデータのアセットを追加

---
+++ Assets/NovelGame/MasterSources/Scripts/Infra/ScenarioDataAsset.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace NovelGame.Master.Scripts.Infra
+{
+    /// <summary>
+    /// ノベルゲームのデータアセットを表します。
+    /// </summary>
+    [CreateAssetMenu(fileName = nameof(ScenarioDataAsset), 
+        menuName = "NovelGame/Master" + nameof(ScenarioDataAsset))]
+    public class ScenarioDataAsset : ScriptableObject
+    {
+
+    }
+}
