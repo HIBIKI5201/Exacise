e9b0787d527c8721d4f36c780f70df0dc36d10c4
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-01 21:58:16 +0900

    [add]アニメを再生するアクションを追加

---
+++ Assets/NovelGame/MasterSources/Scripts/UseCase/Action/ActorAnime.cs
@@ -0,0 +1,36 @@
+using NovelGame.Master.Scripts.Infra;
+using NovelGame.Master.Scripts.UI;
+using NovelGame.Master.Scripts.UseCase;
+using NovelGame.Master.Scripts.Utility;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NovelGame.Master
+{
+    public class ActorAnime : IScenarioAction
+    {
+        public ActorAnime()
+        {
+            _actorName = string.Empty;
+            _clipName = string.Empty;
+        }
+
+        public ActorAnime(string actorName, string clipName)
+        {
+            _actorName = actorName;
+            _clipName = clipName;
+        }
+
+        public async ValueTask ExecuteAsync(ActionRepository repository, IPauseHandler pauseHandler, CancellationToken token = default)
+        {
+            ActorPresenter actor = repository.ActorRepository.GetActorPresenter(_actorName);
+            await actor.PlayAnimationAsync(_clipName, pauseHandler);
+        }
+
+        [SerializeField]
+        private string _actorName;
+        [SerializeField]
+        private string _clipName;
+    }
+}
