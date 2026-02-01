77f9c6794651f405bf40b46665da2b3c3cb2c400
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-01 19:50:27 +0900

    [update]ポーズハンドラを追加

--- Assets/NovelGame/MasterSources/Scripts/UI/SkipWindowPresenter.cs
+++ Assets/NovelGame/MasterSources/Scripts/UI/SkipWindowPresenter.cs
@@ -1,3 +1,4 @@
+using NovelGame.Master.Scripts.Utility;
 using System;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -8,20 +9,36 @@ namespace NovelGame.Master.Scripts.UI
     ///     スキップウィンドウUIを管理します。
     /// </summary>
     [Serializable]
-    public class SkipWindowPresenter
+    public class SkipWindowPresenter : IPauseHandler
     {
+        public bool IsPaused=> _isPaused;
+
         public VisualElement CreateView()
         {
             _root = _visualTreeAsset.Instantiate();
             RootStyleInit(_root.style);
-            _root.style.visibility = Visibility.Hidden;
+
+            _skipRoot = _root.Q<VisualElement>(_skipRootName);
+            Button skipButton = _root.Q<Button>(_skipButtonName);
+            SkipButtonInit(skipButton);
+
+            ChangeVisibility(Visibility.Hidden);
+
             return _root;
         }
 
         [SerializeField]
         private VisualTreeAsset _visualTreeAsset;
 
+        [SerializeField]
+        private string _skipRootName = "skip-root";
+        [SerializeField]
+        private string _skipButtonName = "skip-button";
+
         private VisualElement _root;
+        private VisualElement _skipRoot;
+
+        private bool _isPaused = false;
 
         private void RootStyleInit(IStyle style)
         {
@@ -29,6 +46,22 @@ namespace NovelGame.Master.Scripts.UI
             style.flexGrow = 1;
             style.width = Length.Percent(100);
             style.height = Length.Percent(100);
+        }
+
+        private void SkipButtonInit(Button button)
+        {
+            button.clicked += OnButtonClicked;
+        }
+
+        private void OnButtonClicked()
+        {
+            ChangeVisibility(Visibility.Visible);
+        }
+
+        private void ChangeVisibility(Visibility visibility)
+        {
+            _skipRoot.style.visibility = visibility;
+            _isPaused = visibility == Visibility.Visible;
         }
     }
 }
