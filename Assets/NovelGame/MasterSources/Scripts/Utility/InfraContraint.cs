a11918677a4c7df6ead7a939a64b804183b8a11b
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-01 17:11:15 +0900

    [add]インフラ用の定数値クラスを追加

---
+++ Assets/NovelGame/MasterSources/Scripts/Utility/InfraContraint.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace NovelGame.Master.Scripts.Utility
+{
+    /// <summary>
+    /// インフラ制約を表します。
+    /// </summary>
+    public static class InfraContraint
+    {
+        public const string ASSET_PATH = "NovelGame/Master/";
+    }
+}
