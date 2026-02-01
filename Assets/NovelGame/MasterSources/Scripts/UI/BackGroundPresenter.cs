21e2076002f16d256f9d937b27fb0f70910bff34
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-01 17:07:18 +0900

    [add]名前空間を適用

--- Assets/NovelGame/MasterSources/Scripts/UI/BackGroundPresenter.cs
+++ Assets/NovelGame/MasterSources/Scripts/UI/BackGroundPresenter.cs
@@ -1,16 +1,50 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UI;
+using NovelGame.Scripts;
 
-public class BackGroundPresenter : MonoBehaviour
+namespace NovelGame.Master.Scripts.UI
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    /// <summary>
+    ///     ノベルゲームの背景UIを管理します。
+    /// </summary>
+    public class BackGroundPresenter : MonoBehaviour
     {
-        
-    }
+        [SerializeField] private Image _front;
+        [SerializeField] private Image _back;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public void SetFrontSpriteAsync(Sprite sprite)
+        {
+            _front.sprite = sprite;
+        }
+
+        public async ValueTask CrossFadeAsync(Sprite sprite, float duration, IPauseHandler ph, CancellationToken token = default)
+        {
+            _back.sprite = sprite;
+
+            float elapsed = 0f;
+            while (elapsed < duration)
+            {
+                Color color = _front.color;
+                color.a = 1f - Mathf.Clamp01(elapsed / duration);
+                _front.color = color;
+
+                try
+                {
+                    await Awaitable.NextFrameAsync(token);
+                    await ph.WaitResumeAsync(token);
+                }
+                catch (OperationCanceledException)
+                {
+                    break;
+                }
+                elapsed += Time.deltaTime;
+            }
+
+            _front.sprite = sprite;
+            _front.color = Color.white;
+        }
     }
 }
