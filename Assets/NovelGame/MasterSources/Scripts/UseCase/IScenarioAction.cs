205b610d3045ee6cb58502347af35e0a6fd0f24b
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-01 17:06:32 +0900

    [add]シナリオ用のインターフェースを追加

---
+++ Assets/NovelGame/MasterSources/Scripts/UseCase/IScenarioAction.cs
@@ -0,0 +1,17 @@
+using NovelGame.Master.Scripts.Infra;
+using NovelGame.Master.Scripts.Utility;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NovelGame.Master.Scripts.UseCase
+{
+    /// <summary>
+    ///     ノベルゲームのアクションを表します。
+    /// </summary>
+    public interface IScenarioAction
+    {
+        public ValueTask ExcuteAsync(ActionRepository repository,
+            IPauseHandler pauseHandler,
+            CancellationToken token = default);
+    }
+}
