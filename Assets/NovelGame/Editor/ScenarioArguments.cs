ed1863702a65dd142a7dde83237c76f7ea9b3254
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-03 18:23:48 +0900

    [update]パースに失敗したらデフォルトを返すように

--- Assets/NovelGame/Editor/ScenarioArguments.cs
+++ Assets/NovelGame/Editor/ScenarioArguments.cs
@@ -8,6 +8,7 @@ namespace NovelGame.Master.Scripts.Editor
         }
 
         public Argument this[int index] => new(_args[index]);
+        public int Length => _args.Length;
 
         private readonly string[] _args;
 
@@ -19,9 +20,24 @@ namespace NovelGame.Master.Scripts.Editor
             }
 
             public static implicit operator string(Argument arg) => arg._value;
-            public static implicit operator int(Argument arg) => int.Parse(arg._value);
-            public static implicit operator float(Argument arg) => float.Parse(arg._value);
-            
+            public static implicit operator int(Argument arg)
+            {
+                if (int.TryParse(arg._value, out int result))
+                {
+                    return result;
+                }
+                return default;
+            }
+
+            public static implicit operator float(Argument arg)
+            {
+                if (float.TryParse(arg._value, out float result))
+                {
+                    return result;
+                }
+                return default;
+            }
+
             private readonly string _value;
         }
     }
