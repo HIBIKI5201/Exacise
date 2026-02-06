4c453fbe94f1cecaf5bfecedeefc9f3121df5724
HIBIKI_5201 <takuto5201@gmail.com>
2026-02-06 11:58:26 +0900

    [add]ジャンプ用のボタンのアクションを追加

---
+++ Assets/NovelGame/MasterSources/Scripts/UseCase/Action/ShowButton.cs
@@ -0,0 +1,42 @@
+using NovelGame.Master.Scripts.Infra;
+using NovelGame.Master.Scripts.Utility;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NovelGame.Master.Scripts.UseCase
+{
+    /// <summary>
+    ///     選択肢ボタンを出現させる。
+    /// </summary>
+    public class ShowButton : IScenarioAction
+    {
+        public ShowButton()
+        {
+            _buttonText = string.Empty;
+            _jumpLine = 0;
+            _pos = default;
+        }
+
+        public ShowButton(string buttonText, int jumpLine, Vector2Int pos)
+        {
+            _buttonText = buttonText;
+            _jumpLine = jumpLine;
+            _pos = pos;
+        }
+
+        public ValueTask ExecuteAsync(ActionRepository repository, IPauseHandler pauseHandler, CancellationToken token = default)
+        {
+            repository.NovelUIPresenter.ShowButton(_buttonText, _jumpLine, _pos);
+            return default;
+        }
+
+        [SerializeField]
+        private string _buttonText;
+        [SerializeField]
+        private int _jumpLine;
+        [SerializeField]
+        private Vector2Int _pos;
+
+    }
+}
